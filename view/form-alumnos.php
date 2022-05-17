8d264c8723deb8464159cd0bb644dba90c03d5c9
Joel Vicente <joelvicente@outlook.es>
2022-05-17 10:41:18 +0200

    17/05/2022 10:41

--- view/form-alumnos.php
+++ view/form-alumnos.php
@@ -11,79 +11,80 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <title>Formulario Alumnos</title>
+<!-- Validación por JavaScript -->
+  <script type="text/javascript" src="validaciones.js"></script>  
+  <title>Formulario Alumnos</title>
 </head>
 <body>
     <!-- Formulario de Bootstrap -->
-<form class="needs-validation" novalidate>
+<form class="row g-3 needs-validation" novalidate>
   <div class="form-row">
 
     <!-- Nombre -->
     <div class="col-md-4 mb-3">
-      <label for="AL-validacionNombre">Nombre</label>
-      <input type="text" class="form-control" id="AL-validacionNombre" placeholder="Inserta tu nombre..." required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
+      <label for="validacionNombre">Nombre</label>
+      <input type="text" class="form-control" id="validacionNombre" placeholder="Inserta tu nombre..." required>
     </div>
 
     <!-- Primer apellido -->
     <div class="col-md-4 mb-3">
-      <label for="AL-validacionApellido01">Primer apellido</label>
-      <input type="text" class="form-control" id="AL-validacionApellido01" placeholder="Inserte su apellido..." required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
+      <label for="validacionApellido01">Primer apellido</label>
+      <input type="text" class="form-control" id="validacionApellido01" placeholder="Inserte su apellido..." required>
     </div>
 
     <!-- Segundo apellido -->
     <div class="col-md-4 mb-3">
-      <label for="AL-validacionApellido02">Segundo apellido</label>
-      <input type="text" class="form-control" id="AL-validacionApellido02" placeholder="Inserte su segundo apellido..." required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
+      <label for="validacionApellido02">Segundo apellido</label>
+      <input type="text" class="form-control" id="validacionApellido02" placeholder="Inserte su segundo apellido..." required>
     </div>
 
     <!-- DNI -->
     <div class="col-md-4 mb-3">
-    <label for="AL-validacionDNI"> DNI </label>
-        <input type="text" class="form-control" id="AL-validacionDNI" placeholder="Inserte su DNI..." required>
+    <label for="validacionDNI"> DNI </label>
+        <input type="text" class="form-control" id="validacionDNI" placeholder="Inserte su DNI..." required>
     </div>
 
     <!-- Email -->
     <div class="col-md-4 mb-3">
-            <label for="AL-validacionEmail">Email</label>
-            <input type="email" class="form-control" id="AL-validacionEmail" placeholder="Inserte su Email...">
+            <label for="validacionEmail">Email</label>
+            <input type="email" class="form-control" id="validacionEmail" placeholder="Inserte su Email...">
     </div>
 
     <!-- Teléfono -->
     <div class="col-md-4 mb-3">
-            <label for="AL-validacionTel">Teléfono</label>
-            <input id="AL-validacionTel" class="form-control" type="text" maxlength="5" placeholder="00000...">
+            <label for="validacionTel">Teléfono</label>
+            <input id="validacionTel" class="form-control" type="text" maxlength="5" placeholder="00000...">
     </div>
 
     <!-- Cursos -->
     <div class="form-group col-md-4 mb-3">
     <label>Clase</label><br>
     <select class="custom-select">
-        <option selected>Elige una de las opciones</option>
-        <option value="1">AF (Administración y Finanzas)</option>
-        <option value="2">AI (Cursos Auxiliares de Enfermeria)</option>
-        <option value="3">ASIX1 (1o de Administración de Sistemas Informáticos y Redes)</option>
-        <option value="4">ASIX2 (2o de Administración de Sistemas Informáticos y Redes)</option>
-        <option value="5">DAW (Desarrollo de Aplicaciones Web)</option>
-        <option value="6">EAS (Enseñanza y Animación Sociodeportiva)</option>
-        <option value="7">EF (Eduación Infantil)</option>
-        <option value="8">GA (Gestión Administrativa)</option>
-        <option value="9">HBD (Higiene Bucodental)</option>
-        <option value="10">SMX1 (1r de Sistemas Microinformáticos y Redes)</option>
-        <option value="11">SMX2 (2o de Sistemas Microinformáticos y Redes)</option>
-        <option value="12">Guía (Guía en el Medio Natural y Tiempo Libre)</option>
+        <option selected disabled value="">Elige una de las opciones</option>
+        <option value="1">AF1 (1o de Administración y Finanzas)</option>
+        <option value="2">AF2 (2o de Administración y Finanzas)</option>
+        <option value="3">AI1 (1o de Cursos Auxiliares de Enfermeria)</option>
+        <option value="4">AI2 (2o de Cursos Auxiliares de Enfermeria)</option>
+        <option value="5">ASIX1/DAW1 (1o de Administración de Sistemas Informáticos y Redes / 1o de Desarrollo de Aplicaciones Web)</option>
+        <option value="6">ASIX2 (2o de Administración de Sistemas Informáticos y Redes)</option>
+        <option value="7">DAW2 (2o de Desarrollo de Aplicaciones Web)</option>
+        <option value="8">EAS1 (1o de Enseñanza y Animación Sociodeportiva)</option>
+        <option value="9">EAS2 (2o de Enseñanza y Animación Sociodeportiva)</option>
+        <option value="10">EF1 (1o de Eduación Infantil)</option>
+        <option value="11">EF2 (2o de Eduación Infantil)</option>
+        <option value="12">GA1 (1o de Gestión Administrativa)</option>
+        <option value="13">GA2 (2o de Gestión Administrativa)</option>
+        <option value="14">HBD1 (1o de Higiene Bucodental)</option>
+        <option value="15">HBD2 (2o de Higiene Bucodental)</option>
+        <option value="16">SMX1 (1r de Sistemas Microinformáticos y Redes)</option>
+        <option value="17">SMX2 (2o de Sistemas Microinformáticos y Redes)</option>
+        <option value="18">1o de Guía (Guía en el Medio Natural y Tiempo Libre)</option>
+        <option value="19">2o de Guía (Guía en el Medio Natural y Tiempo Libre)</option>
     </select>
     </div>
-
-  <button class="btn btn-primary" type="submit">Enviar</button>
 </form>
+<form action="../index.php" method="POST" enctype="multipart/form-data"  method="" action="" onsubmit="return validaFormulario();">
+    <input type="submit" class="btn btn-primary" value="Volver">
+    </form>
 </body>
 </html>
--- view/form-profesores.php
+++ view/form-profesores.php
@@ -11,73 +11,74 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <title>Formulario Profesores</title>
+  <!-- Validación por JavaScript   -->
+  <script type="text/javascript" src="validaciones.js"></script>
+  <title>Formulario Profesores</title>
 </head>
 <body>
     <!-- Formulario de Bootstrap -->
-<form class="needs-validation" novalidate>
+<form class="row g-3 needs-validation" novalidate>
   <div class="form-row">
 
     <!-- Nombre -->
     <div class="col-md-4 mb-3">
-      <label for="PR-validacionNombre">Nombre</label>
-      <input type="text" class="form-control" id="PR-validacionNombre" placeholder="Inserta tu nombre..." required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
+      <label for="validacionNombre" class="form-control">Nombre</label>
+      <input type="text" class="form-control" id="validacionNombre" placeholder="Inserta tu nombre..." required>
     </div>
 
     <!-- Primer apellido -->
     <div class="col-md-4 mb-3">
-      <label for="PR-validacionApellido01">Primer apellido</label>
-      <input type="text" class="form-control" id="PR-validacionApellido01" placeholder="Inserte su apellido..." required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
+      <label for="validacionApellido01">Primer apellido</label>
+      <input type="text" class="form-control" id="validacionApellido01" placeholder="Inserte su apellido..." required>
     </div>
 
     <!-- Segundo apellido -->
     <div class="col-md-4 mb-3">
-      <label for="PR-validacionApellido02">Segundo apellido</label>
-      <input type="text" class="form-control" id="PR-validacionApellido02" placeholder="Inserte su segundo apellido..." required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
+      <label for="validacionApellido02">Segundo apellido</label>
+      <input type="text" class="form-control" id="validacionApellido02" placeholder="Inserte su segundo apellido..." required>
     </div>
 
     <!-- Email -->
     <div class="col-md-4 mb-3">
-            <label for="PR-validacionEmail">Email</label>
-            <input type="email" class="form-control" id="PR-validacionEmail" placeholder="Inserte su Email...">
+            <label for="validacionEmail">Email</label>
+            <input type="email" class="form-control" id="validacionEmail" placeholder="Inserte su Email...">
     </div>
 
     <!-- Teléfono -->
     <div class="col-md-4 mb-3">
-            <label for="PR-validacionTel">Teléfono</label>
-            <input id="PR-validacionTel" class="form-control" type="text" maxlength="5" placeholder="00000...">
+            <label for="validacionTel">Teléfono</label>
+            <input id="validacionTel" class="form-control" type="text" maxlength="5" placeholder="00000...">
     </div>
 
     <!-- Cursos -->
     <div class="form-group col-md-4 mb-3">
     <label>Grado</label>
     <select class="custom-select">
-        <option selected>Elige una de las opciones</option>
-        <option value="1">AF (Administración y Finanzas)</option>
-        <option value="2">AI (Cursos Auxiliares de Enfermeria)</option>
-        <option value="3">ASIX1 (1o de Administración de Sistemas Informáticos y Redes)</option>
-        <option value="4">ASIX2 (2o de Administración de Sistemas Informáticos y Redes)</option>
-        <option value="5">DAW (Desarrollo de Aplicaciones Web)</option>
-        <option value="6">EAS (Enseñanza y Animación Sociodeportiva)</option>
-        <option value="7">EF (Eduación Infantil)</option>
-        <option value="8">GA (Gestión Administrativa)</option>
-        <option value="9">HBD (Higiene Bucodental)</option>
-        <option value="10">SMX1 (1r de Sistemas Microinformáticos y Redes)</option>
-        <option value="11">SMX2 (2o de Sistemas Microinformáticos y Redes)</option>
-        <option value="12">Guía (Guía en el Medio Natural y Tiempo Libre)</option>
+    <option selected disabled value="">Elige una de las opciones</option>
+        <option value="1">AF1 (1o de Administración y Finanzas)</option>
+        <option value="2">AF2 (2o de Administración y Finanzas)</option>
+        <option value="3">AI1 (1o de Cursos Auxiliares de Enfermeria)</option>
+        <option value="4">AI2 (2o de Cursos Auxiliares de Enfermeria)</option>
+        <option value="5">ASIX1/DAW1 (1o de Administración de Sistemas Informáticos y Redes / 1o de Desarrollo de Aplicaciones Web)</option>
+        <option value="6">ASIX2 (2o de Administración de Sistemas Informáticos y Redes)</option>
+        <option value="7">DAW2 (2o de Desarrollo de Aplicaciones Web)</option>
+        <option value="8">EAS1 (1o de Enseñanza y Animación Sociodeportiva)</option>
+        <option value="9">EAS2 (2o de Enseñanza y Animación Sociodeportiva)</option>
+        <option value="10">EF1 (1o de Eduación Infantil)</option>
+        <option value="11">EF2 (2o de Eduación Infantil)</option>
+        <option value="12">GA1 (1o de Gestión Administrativa)</option>
+        <option value="13">GA2 (2o de Gestión Administrativa)</option>
+        <option value="14">HBD1 (1o de Higiene Bucodental)</option>
+        <option value="15">HBD2 (2o de Higiene Bucodental)</option>
+        <option value="16">SMX1 (1r de Sistemas Microinformáticos y Redes)</option>
+        <option value="17">SMX2 (2o de Sistemas Microinformáticos y Redes)</option>
+        <option value="18">1o de Guía (Guía en el Medio Natural y Tiempo Libre)</option>
+        <option value="19">2o de Guía (Guía en el Medio Natural y Tiempo Libre)</option>
     </select>
     </div>
-
-  <button class="btn btn-primary" type="submit">Enviar</button>
 </form>
+  <form action="../index.php" method="POST" enctype="multipart/form-data"  method="" action="" onsubmit="return validaFormulario();">
+    <input type="submit" class="btn btn-primary" value="Volver">
+    </form>
 </body>
 </html>
